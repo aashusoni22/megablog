e0fd21d19b6f0d467ce156f3ebce145e41b5dfa4
Aashutosh Soni <114427519+aashusoni22@users.noreply.github.com>
2024-09-03 12:21:56 -0400

    conf completed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="p-3 bg-slate-900 text-3xl font-semibold text-center text-cyan-500">
+        A blog app in appwrite
+      </h1>
+    </>
+  );
+}
+
+export default App;
