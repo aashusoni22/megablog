c9f5d56eb1f1bde4b1ad5403c1dccb7422e47525
Aashutosh Soni <114427519+aashusoni22@users.noreply.github.com>
2024-09-08 01:16:56 -0400

    Restore .gitignore file

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,17 +3,14 @@ import React from "react";
 function Button({
   children,
   type = "button",
-  bgColor = "bg-cyan-400",
-  textColor = "text-white",
+  bgColor = "bg-cyan-500",
+  textColor = "text-slate-800",
   className = "",
-  disabled = false,
   ...props
 }) {
   return (
     <button
-      type={type}
-      className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className} transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 disabled:opacity-50 disabled:cursor-not-allowed`}
-      disabled={disabled}
+      className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}
       {...props}
     >
       {children}
